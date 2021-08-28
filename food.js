b70a11ffe7f2bc87b1256380e05cfe4eb5f66805
Yahavarshini Elamurugu <85391841+Yahavarshini@users.noreply.github.com>
2021-08-28 15:22:45 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,38 @@
+let foods=[
+    {
+        name:"tomato",
+        price: 50,
+        quantity: "1kg"
+    }
+    ,
+    {
+        name:"potato",
+        price: 30,
+        quantity: "500g"
+    }
+    ,
+    
+    {
+        name:"big-onion",
+        price: 50,
+        quantity: "1kg"
+    }
+    ,
+     
+    {
+        name:"small-onion",
+        price: 80,
+        quantity: "1kg"
+    }
+    ,
+    {
+        name:"carrot",
+        price: 70,
+        quantity: "500g"
+    }
+
+]
+
+for(let num=0;num<foods.length;num++){
+    console.log(foods[num]);
+}
